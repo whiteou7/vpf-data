c5b3586ee6e35b26c43ab7cd6309ff22af0412f8
whiteou7 <fps4day@gmail.com>
2025-09-17 00:05:04 +0700

    make the height of table in index page scale with screen height

--- src/components/LiftersPBTable.vue
+++ src/components/LiftersPBTable.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import BaseTable from "./BaseTable.vue"
 
+const height = ref(window.innerHeight - 200)
+
+const updateHeight = () => {
+  height.value = window.innerHeight - 200
+}
+
+onMounted(() => {
+  window.addEventListener("resize", updateHeight)
+})
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateHeight)
+})
+
 </script>
 
 <template>
   <BaseTable
-    height="600"
+    :height="height"
   >
     <template #top>
       <slot name="top" />
