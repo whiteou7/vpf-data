7c5d84c3f8e9845ddd7d5a4520ee6b13dd1e4776
whiteou7 <fps4day@gmail.com>
2025-09-29 23:04:07 +0700

    feat: image cropper

--- src/pages/athlete/[vpf_id]/personal-info.vue
+++ src/pages/athlete/[vpf_id]/personal-info.vue
@@ -1,24 +1,62 @@
 <script setup lang="ts">
 import type { APIBody } from "~/types/api"
 import type { FormTextFieldConfig } from "~/types/component"
+import VuePictureCropper, { cropper } from "vue-picture-cropper"
 
 const { personalInfo } = useFetchAthlete()
 const fieldConfig: Array<FormTextFieldConfig> = [
   { value: "email", label: "Email", icon: "mdi-email-outline", type: "email" },
   { value: "nationality", label: "Nationality", icon: "mdi-flag" },
   { value: "dob", label: "Date of Birth", icon: "mdi-calendar-range" },
-  { value: "nationalId", label: "National ID", icon: "mdi-card-account-details", type: "number" },
+  { value: "nationalId", label: "National ID", icon: "mdi-card-account-details", type: "text-digit-only" },
   { value: "address", label: "Address", icon: "mdi-home" },
-  { value: "phoneNumber", label: "Phone Number", icon: "mdi-phone", type: "number" }
+  { value: "phoneNumber", label: "Phone Number", icon: "mdi-phone", type: "text-digit-only" }
 ]
 const snackbar = useSnackbar()
-const nationalIdImage = ref<File>()
-const previewUrl = computed(() =>
-  nationalIdImage.value ? window.URL.createObjectURL(nationalIdImage.value) : ""
-)
-const dialog = ref(false)
+
+const version = ref<number>(0)
+
+const nationalIdImage = ref<File | undefined>()
+const croppedImageUrl = ref<string | undefined>()
+
+// Image cropper related refs
+const isCropperModalOpen = ref<boolean>(false)
+const uncroppedImageUrl = ref<string | undefined>("") // unprocessed base64 URL fed to cropper
+
+const isUploadModalOpen = ref(false)
 const route = useRoute()
 const vpfId = route.params.vpf_id as string
+
+// ChatGPT wrote this shit
+// Init necessary variables and open modal
+const prepareCropping = () => {
+  if (!nationalIdImage.value) return
+  
+  uncroppedImageUrl.value = ""
+  croppedImageUrl.value = ""
+
+  const reader = new FileReader()
+  reader.readAsDataURL(nationalIdImage.value)
+  reader.onload = () => {
+    uncroppedImageUrl.value = String(reader.result)
+    isCropperModalOpen.value = true
+  }
+}
+
+// Get data from cropper and store to previewUrl as well as national id pic
+const handleCropping = async () => {
+  if (!cropper) return
+
+  const base64 = cropper.getDataURL()
+  croppedImageUrl.value = base64
+  isCropperModalOpen.value = false
+  
+  // Convert base64 to File object
+  const response = await fetch(base64)
+  const blob = await response.blob()
+  const file = new File([blob], "national-id.jpg", { type: "image/jpeg" })
+  nationalIdImage.value = file
+}
 
 const handleUpload = async () => {
   if (!nationalIdImage.value) return
@@ -37,8 +75,9 @@ const handleUpload = async () => {
       text: response.message ?? "",
       color: "success",
     })
-    // Refetch to display new image
-    await useFetchAthlete().fetch(vpfId, true)
+
+    // Re-render the image component
+    version.value++
   } else {
     snackbar.push({
       text: response.message,
@@ -46,7 +85,7 @@ const handleUpload = async () => {
     })
   }
   
-  dialog.value = false
+  isUploadModalOpen.value = false
   
 }
 
@@ -77,7 +116,7 @@ const submit = async () => {
         <DataForm
           :model="personalInfo"
           :fields="fieldConfig"
-          :onSubmit="submit"
+          :on-submit="submit"
         />
       </v-card-text>
       <v-card-text v-else>
@@ -86,10 +125,11 @@ const submit = async () => {
     </v-card>
 
     <v-card max-width="500" style="flex: 1;" title="National ID">
+      <!-- Display national ID image/placeholder text -->
       <v-card-text>
         <div v-if="personalInfo?.nationalIdImageUrl">
           <v-img
-            :src="personalInfo.nationalIdImageUrl"
+            :src="`${personalInfo.nationalIdImageUrl}?version=${version}`"
             aspect-ratio="16/9"
             alt="National ID"
             class="rounded-lg"
@@ -111,55 +151,98 @@ const submit = async () => {
           </v-alert>
         </div>
       </v-card-text>
+
+      <!-- Button to edit/upload -->
       <v-card-actions class="d-flex justify-end">
         <div class="text-center px-4">
-          <v-btn class="px-4" @click="dialog = true">
+          <v-btn class="px-4" @click="isUploadModalOpen = true">
             Edit/Upload National ID
           </v-btn>
-
-          <v-dialog
-            v-model="dialog"
-            width="auto"
-          >
-            <v-card
-              width="600"
-              class="pa-4"
-            >
-              <v-file-upload 
-                v-if="!nationalIdImage"
-                v-model="nationalIdImage"
-                accept="image/*"
-              />
-              <v-img
-                v-else
-                :src="previewUrl"
-                aspect-ratio="16/9"
-                alt="National ID Preview"
-                class="rounded-lg"
-              />
-              <template #actions>
-                <v-btn
-                  class="mx-auto"
-                  text="Cancel"
-                  @click="dialog = false"
-                />
-                <v-btn
-                  v-if="nationalIdImage"
-                  class="mx-auto"
-                  text="Retry"
-                  @click="nationalIdImage = undefined"
-                />
-                <v-btn
-                  v-if="nationalIdImage"
-                  class="mx-auto"
-                  text="Upload"
-                  @click="handleUpload"
-                />
-              </template>
-            </v-card>
-          </v-dialog>
         </div>
       </v-card-actions>
     </v-card>
+
+    <!-- Modal for uploading image -->
+    <v-dialog
+      v-model="isUploadModalOpen"
+      width="auto"
+      persistent
+    >
+      <v-card
+        width="600"
+        class="pa-4"
+      >
+        <v-card-title v-if="croppedImageUrl" class="pb-2 pt-0 px-0">Preview Image</v-card-title>
+        <v-file-upload 
+          v-if="!croppedImageUrl"
+          v-model="nationalIdImage"
+          accept="image/*"
+          @update:model-value="prepareCropping"
+        />
+        
+        <!-- Show preview of cropped image -->
+        <v-img
+          v-if="croppedImageUrl"
+          :src="croppedImageUrl"
+          aspect-ratio="16/9"
+          alt="National ID Preview"
+          class="rounded-lg"
+        />
+        
+        <template #actions>
+          <v-btn
+            v-if="croppedImageUrl"
+            class="mx-auto"
+            text="Retry"
+            @click="croppedImageUrl = undefined; uncroppedImageUrl = undefined; nationalIdImage = undefined"
+          />
+          <v-btn
+            class="mx-auto"
+            text="Cancel"
+            @click="isUploadModalOpen = false"
+          />
+          <v-btn
+            v-if="nationalIdImage"
+            class="mx-auto"
+            text="Upload"
+            @click="handleUpload"
+          />
+        </template>
+      </v-card>
+    </v-dialog>
+
+    <!-- Cropper Modal -->
+    <v-dialog v-model="isCropperModalOpen" width="auto" persistent>
+      <v-card>
+        <v-card-title>Crop Image</v-card-title>
+        <v-card-text class="px-4 py-0">
+          <div>
+            <VuePictureCropper
+              :box-style="{
+                width: '100%',
+                height: '100%',
+                backgroundColor: '#f8f8f8',
+                margin: 'auto',
+              }"
+              :img="uncroppedImageUrl"
+              :options="{
+                viewMode: 1,
+                dragMode: 'crop',
+                aspectRatio: 16 / 9,
+              }"
+            />
+          </div>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn color="primary" text="Crop Image" @click="handleCropping" />
+          <v-btn
+            class="mx-auto"
+            text="Cancel"
+            @click="isUploadModalOpen = false; isCropperModalOpen = false; nationalIdImage = undefined"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
+
 </template>
