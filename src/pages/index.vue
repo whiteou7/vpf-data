93ca5518d7b936d9b12b37f2cc9900baf7c50616
whiteou7 <fps4day@gmail.com>
2025-10-02 00:53:56 +0700

    feat: optimize infinite scroll index table by not loading huge dataset at once

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <AthletesCompTable
+    ref="tableRef"
     :items="filteredAthletes"
     :headers="headers"
     :loading="loading"
     :search="filters.search.value"
+    @update:sortBy="handleSort"
   >
     <template #top>
       <AthletesFilter />
@@ -12,30 +14,77 @@
 </template>
 
 <script setup lang="ts">
-import type { AthleteCompInfo } from "~/types/athlete"
+import type { Athlete } from "~/types/athlete"
 import AthletesCompTable from "~/components/AthletesCompTable.vue"
 import AthletesFilter from "~/components/AthletesFilter.vue"
 import { useAthletesFilter } from "~/composables/useAthletesFilter"
+import type { APIBody } from "~/types/api"
 
-// Fetch
-const athletes = ref<AthleteCompInfo[]>([])
+const athletes = ref<Athlete[]>([])
 const loading = ref(true)
 
 const filters = useAthletesFilter()
+const visibleCount = ref(50)
+
+const tableRef = ref()
+
+// Load more item on the end of table
+function handleScroll(e: Event) {
+  const el = e.target as HTMLElement
+  if (el.scrollTop + el.clientHeight >= el.scrollHeight - 10) {
+    if (visibleCount.value < athletes.value.length) {
+      visibleCount.value += 50
+    }
+  }
+}
+
+// Load all item into table if sort, will improve later
+function handleSort(e: Array<{ key: string, order: string}>) {
+  if (e.length) {
+    visibleCount.value = 999
+  } else {
+    visibleCount.value = 50
+  }
+}
+
+// Reset visible count upon filter actions
+watch(
+  () => [filters.sexFilter.value, filters.divisionFilter.value, filters.weightClassFilter.value, filters.search.value],
+  () => {
+    visibleCount.value = 50
+  }
+)
 
 onMounted(async () => {  
-  const response = await $fetch("/api/athletes", { ignoreResponseError: true })
+  // Fetch
+  const response = await $fetch<APIBody<{ athletes: Athlete[] }>>("/api/athletes", { ignoreResponseError: true })
   if (!response.success) {
     // TODO: Handle error
     return
   }
   loading.value = false
-  athletes.value = response.data?.athletes ?? []
+  athletes.value = response.data.athletes
+
+  // Manually add scroll event to table
+  const rootEl = tableRef.value?.$el as HTMLElement
+  if (!rootEl) return
+
+  const wrapper = rootEl.querySelector(".v-table__wrapper") as HTMLElement
+  if (wrapper) {
+    wrapper.addEventListener("scroll", handleScroll)
+  }
+})
+
+// Remove event
+onBeforeUnmount(() => {
+  const rootEl = tableRef.value?.$el as HTMLElement
+  const wrapper = rootEl?.querySelector(".v-table__wrapper") as HTMLElement
+  wrapper?.removeEventListener("scroll", handleScroll)
 })
 
 // Computed filtering logic
 const filteredAthletes = computed(() => {
-  return athletes.value.filter(athlete => {
+  const result = athletes.value.filter(athlete => {
     const matchesSex = filters.sexFilter.value ? athlete.sex === filters.sexFilter.value : true
     const matchesDivision = filters.divisionFilter.value ? athlete.division === filters.divisionFilter.value : true
     const matchesWeightClass = filters.weightClassFilter.value.weight
@@ -43,6 +92,8 @@ const filteredAthletes = computed(() => {
       : true
     return matchesSex && matchesDivision && matchesWeightClass
   })
+
+  return result.slice(0, visibleCount.value) // only show up to visibleCount
 })
 
 const headers = [
