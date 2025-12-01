a6c9b577c3c19645cc845dd49cfe1926dcdcda90
whiteou7 <fps4day@gmail.com>
2025-12-01 19:52:19 +0700

    feat: change icon

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -21,7 +21,8 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1.0" }
-      ]
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
     }
   },
   ssr: false,
